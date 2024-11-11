cb192520ed0faa9a06341b2dfb146f963633c5af
Windows <schwenkmark2007@gmail.com>
2024-11-11 15:50:53 -0800

    adds main classes

--- main.cpp
+++ main.cpp
@@ -1,10 +1,38 @@
 #include <iostream>
 #include <string>
 
+class ChessBoard {
+    public:
+    void printBoard()
+    {
+
+    }
+};
+
+class ChessPiece{
+    public:
+    ChessPiece()
+    {
+        for(int i; i < 8; i++)
+        {
+            for(int j; j < 8; j++)
+            {
+                arrChess[i][j] = 0;
+            }
+        }    
+    }
+    bool isValidMove(int startX, int startY, int endX, int endY)
+    {
+        
+    }
+    protected:
+    int arrChess[8][8];
+
+};
+
+
 int main()
 {
-    std::string d;
-    std::cin >> d;
-    std::cout << d;
+    
     return 0;
 }
